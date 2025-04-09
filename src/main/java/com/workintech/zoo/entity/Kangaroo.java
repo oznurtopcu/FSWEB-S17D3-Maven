835a59f100cda5c6bbaab0fcba27c6819dfe1a79
oznurtopcu <topcuoznur121@gmail.com>
2025-04-09 16:25:09 +0300

    create kangaroo class

--- src/main/java/com/workintech/zoo/entity/Kangaroo.java
+++ src/main/java/com/workintech/zoo/entity/Kangaroo.java
@@ -1,4 +1,17 @@
 package com.workintech.zoo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Kangaroo {
+    private long id;
+    private String name;
+    private double height;
+    private double weight;
+    private String gender;
+    private Boolean isAggressive;
 }
