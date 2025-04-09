b413530024802f6ececf2351e62c07e3a1ae6e04
oznurtopcu <topcuoznur121@gmail.com>
2025-04-09 16:26:28 +0300

    create ZooException class

--- src/main/java/com/workintech/zoo/exceptions/ZooException.java
+++ src/main/java/com/workintech/zoo/exceptions/ZooException.java
@@ -1,4 +1,19 @@
 package com.workintech.zoo.exceptions;
 
-public class ZooException {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+//httpStatus için getter yazdık
+
+@Getter
+@Setter
+public class ZooException extends RuntimeException {
+
+    private HttpStatus httpStatus;
+
+    public ZooException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
 }
