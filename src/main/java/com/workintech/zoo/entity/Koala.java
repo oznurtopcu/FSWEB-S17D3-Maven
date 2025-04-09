d104486e00b7bbf9093ad679bdcdd30bcd5a78d3
oznurtopcu <topcuoznur121@gmail.com>
2025-04-09 16:25:20 +0300

    create koala class

--- src/main/java/com/workintech/zoo/entity/Koala.java
+++ src/main/java/com/workintech/zoo/entity/Koala.java
@@ -1,4 +1,18 @@
 package com.workintech.zoo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Component
 public class Koala {
+    private long id;
+    private String name;
+    private double sleepHour;
+    private double weight;
+    private String gender;
 }
